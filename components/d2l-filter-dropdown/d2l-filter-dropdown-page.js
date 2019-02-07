af0b79e577fa97d6a161314dc6abcb10ea99f499
Carol Busman <Carol.Busman@D2L.com>
2019-02-07 10:51:01 -0500

    Separating styles into their own files.

--- components/d2l-filter-dropdown/d2l-filter-dropdown-page.js
+++ components/d2l-filter-dropdown/d2l-filter-dropdown-page.js
@@ -4,6 +4,7 @@ import '@polymer/iron-input/iron-input.js';
 import 'd2l-icons/d2l-icon.js';
 import 'd2l-icons/tier1-icons.js';
 import 'd2l-icons/tier2-icons.js';
+import './d2l-filter-dropdown-page-styles.js';
 import './d2l-filter-dropdown-localize-behavior.js';
 
 /**
@@ -14,93 +15,7 @@ import './d2l-filter-dropdown-localize-behavior.js';
 class D2LFilterDropdownPage extends mixinBehaviors([D2L.PolymerBehaviors.FilterDropdown.LocalizeBehavior], PolymerElement) {
 	static get template() {
 		return html`
-			<style>
-				:host {
-					--d2l-filter-dropdown-page-search-height: 45px;
-				}
-				:host iron-input {
-					display: block;
-				}
-				.d2l-filter-dropdown-option:hover {
-					background-color: var(--d2l-color-celestine-plus-2);
-					border-top-color: var(--d2l-color-celestine-plus-1);
-					border-bottom-color: var(--d2l-color-celestine-plus-1);
-					color: var(--d2l-color-celestine);
-				}
-				.d2l-filter-dropdown-option {
-					cursor: pointer;
-					border: 1px solid transparent;
-					color: var(--d2l-color-ferrite);
-					line-height: 1.2rem;
-					font-size: 0.8rem;
-					padding: 0.5rem 1rem;
-				}
-				.d2l-filter-dropdown-option:not([hidden]) ~ .d2l-filter-dropdown-option:not([hidden]) {
-					border-top-color: var(--d2l-color-gypsum);
-				}
-				.d2l-filter-dropdown-option:not([hidden]) ~ .d2l-filter-dropdown-option:not([hidden]):hover {
-					border-top-color: var(--d2l-color-celestine-plus-1);
-					border-bottom-color: var(--d2l-color-celestine-plus-1);
-				}
-				.d2l-filter-dropdown-option d2l-icon {
-					--d2l-icon-height: 1rem;
-					--d2l-icon-width: 1rem;
-					visibility: visible;
-					margin-right: 0.5rem;
-				}
-				.d2l-filter-dropdown-page-search {
-					position: relative;
-					margin: 10px 20px;
-				}
-				.d2l-filter-dropdown-page-search-input {
-					color: var(--d2l-color-ferrite);
-					font-family: inherit;
-					font-size: calc(var(--d2l-filter-dropdown-page-search-height) / 3) !important;
-					width: 100%;
-					height: var(--d2l-filter-dropdown-page-search-height);
-					border: 1px solid var(--d2l-color-titanius);
-					border-radius: .3rem;
-					box-shadow: inset 0 2px 0 0 rgba(185,194,208,.2);
-					box-sizing: border-box;
-					transition: background-color 0.5s ease, border-color 0.5s ease;
-					-webkit-transition: background-color 0.5s ease, border-color 0.5s ease;
-					padding-left: calc(var(--d2l-filter-dropdown-page-search-height) / 6);
-					padding-right: var(--d2l-filter-dropdown-page-search-height);
-				}
-				.d2l-filter-dropdown-page-search-input:focus,
-				.d2l-filter-dropdown-page-search-input:hover {
-					border-color: var(--d2l-color-celestine);
-					border-width: 2px;
-					outline: 0;
-					/* Subtract 1px for increased border width */
-					padding-left: calc(var(--d2l-filter-dropdown-page-search-height) / 6 - 1px);
-					padding-right: calc(var(--d2l-filter-dropdown-page-search-height) - 1px);
-				}
-				.d2l-filter-dropdown-page-search-input::-ms-clear {
-					display: none;
-				}
-				.d2l-filter-dropdown-clear-button {
-					position: absolute;
-					top: 0px;
-					right: 0px;
-					margin: calc(var(--d2l-filter-dropdown-page-search-height) / 6);
-					background: none;
-					border: 1px solid transparent;
-					cursor: pointer;
-					height: calc(2 * var(--d2l-filter-dropdown-page-search-height) / 3);
-					width: calc(2 * var(--d2l-filter-dropdown-page-search-height) / 3);
-				}
-				.d2l-filter-dropdown-search-clear:hover,
-				.d2l-filter-dropdown-search-clear:focus {
-					border-color: var(--d2l-color-pressicus);
-					border-radius: 0.3rem;
-				}
-				.d2l-filter-dropdown-search-clear d2l-icon {
-					--d2l-icon-height: calc(var(--d2l-filter-dropdown-page-search-height) / 3);
-					--d2l-icon-width: calc(var(--d2l-filter-dropdown-page-search-height) / 3);
-					color: var(--d2l-color-ferrite);
-				}
-			</style>
+			<style include="d2l-filter-dropdown-page-styles"></style>
 			<div class="d2l-filter-dropdown-page-search" hidden$="[[disableSearch]]">
 				<iron-input bind-value="{{_searchInput}}" class="d2l-filter-dropdown-search-box">
 					<input class="d2l-filter-dropdown-page-search-input" value="{{_searchInput}}" placeholder="[[localize('searchBy', 'category', parentTitle)]]" on-keydown="_onSearchChanged">
---
+++ components/d2l-filter-dropdown/d2l-filter-dropdown-styles.js
@@ -0,0 +1,82 @@
+import '@polymer/polymer/polymer-legacy.js';
+import 'd2l-colors/d2l-colors.js';
+const $_documentContainer = document.createElement('template');
+
+$_documentContainer.innerHTML = `<dom-module id="d2l-filter-dropdown-styles">
+	<template strip-whitespace="">
+		<style>
+			d2l-dropdown {
+				min-width: 100%;
+			}
+			d2l-dropdown-content {
+				--d2l-dropdown-verticaloffset: 5px;
+			}
+			.d2l-dropdown-opener {
+				background: none;
+				border: none;
+				cursor: pointer;
+				outline: none;
+				font-size: 0.95rem;
+				font-family: Lato;
+				padding: 0;
+				margin-left: 1rem;
+			}
+			.d2l-filter-dropdown-tab {
+				display: inline-block;
+			}
+			.d2l-filter-dropdown-clear-button {
+				color: var(--d2l-color-celestine);
+				background: none;
+				border: none;
+				cursor: pointer;
+				margin: 0 !important;
+				padding: 0;
+			}
+			.d2l-filter-dropdown-content-header {
+				box-sizing: border-box;
+				display: flex;
+				justify-content: space-between;
+				border-bottom: 1px solid var(--d2l-color-titanius);
+				width: 100%;
+				padding: 20px;
+			}
+			.d2l-filter-dropdown-tab-button {
+				background: none;
+				border: none;
+				color: var(--d2l-color-ferrite);
+				cursor: pointer;
+				display: inherit;
+				font-family: inherit;
+				outline: none;
+				padding: 10px;
+			}
+			.d2l-filter-dropdown-tab-highlight {
+				background-color: var(--d2l-color-celestine);
+				border-bottom-left-radius: 4px;
+				border-bottom-right-radius: 4px;
+				height: 4px;
+				margin: auto;
+				width: 80%;
+			}
+			.d2l-filter-dropdown-tab-selector {
+				display: block;
+				align-items: center;
+				overflow-x: auto;
+				white-space: nowrap;
+				width: 100%;
+				height: 100%;
+				background: linear-gradient(to top, white, #f9fafb);
+			}
+			
+			button:focus > d2l-icon,
+			button:hover > d2l-icon,
+			button:focus > span,
+			button:hover > span {
+				text-decoration: underline;
+				color: var(--d2l-color-celestine);
+			}
+		</style>
+	</template>
+</dom-module>`;
+
+document.head.appendChild($_documentContainer.content);
