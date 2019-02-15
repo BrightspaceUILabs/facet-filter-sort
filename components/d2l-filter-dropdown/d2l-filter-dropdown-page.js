be00a4a19257074c73219f3f769f6531e9825835
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2019-02-15 10:13:34 -0500

    Some demo cleanup to use d2l-typography and get demos working in Edge

--- components/d2l-filter-dropdown/d2l-filter-dropdown-page.js
+++ components/d2l-filter-dropdown/d2l-filter-dropdown-page.js
@@ -1,7 +1,6 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
 import 'd2l-icons/d2l-icon.js';
-import 'd2l-icons/tier1-icons.js';
 import 'd2l-icons/tier2-icons.js';
 import 'd2l-inputs/d2l-input-search.js';
 import './d2l-filter-dropdown-page-styles.js';
