ac5c843d631b551ec8f2dc163dedf57d9d567826
Stacey Van Herk <Stacey.VanHerk@D2L.com>
2019-02-20 22:16:52 -0500

    Use d2l-menu

--- components/d2l-filter-dropdown/d2l-filter-dropdown-page-styles.js
+++ components/d2l-filter-dropdown/d2l-filter-dropdown-page-styles.js
@@ -5,33 +5,9 @@ const $_documentContainer = document.createElement('template');
 $_documentContainer.innerHTML = `<dom-module id="d2l-filter-dropdown-page-styles">
 	<template strip-whitespace="">
 		<style>
-			.d2l-filter-dropdown-option {
-				border: 1px solid transparent;
-				color: var(--d2l-color-ferrite);
-				cursor: pointer;
-				font-size: 0.8rem;
-				line-height: 1.2rem;
+			d2l-menu {
 				margin: 0 -1rem;
-				padding: 0.5rem 1rem;
-			}
-			.d2l-filter-dropdown-option:hover {
-				background-color: var(--d2l-color-celestine-plus-2);
-				border-top-color: var(--d2l-color-celestine-plus-1);
-				border-bottom-color: var(--d2l-color-celestine-plus-1);
-				color: var(--d2l-color-celestine);
-			}
-			.d2l-filter-dropdown-option:not([hidden]) ~ .d2l-filter-dropdown-option:not([hidden]) {
-				border-top-color: var(--d2l-color-gypsum);
-			}
-			.d2l-filter-dropdown-option:not([hidden]) ~ .d2l-filter-dropdown-option:not([hidden]):hover {
-				border-top-color: var(--d2l-color-celestine-plus-1);
-				border-bottom-color: var(--d2l-color-celestine-plus-1);
-			}
-			.d2l-filter-dropdown-option d2l-icon {
-				--d2l-icon-height: 1rem;
-				--d2l-icon-width: 1rem;
-				margin-right: 0.5rem;
-				visibility: visible;
+				width: unset;
 			}
 			.d2l-filter-dropdown-page-search {
 				margin-bottom: 0.5rem;
