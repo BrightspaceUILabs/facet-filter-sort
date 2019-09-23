e9a0f7d00f782c6890ece1373aee150b0cdea17a
Mohamed <mohamed.aboelsoud@d2l.com>
2019-09-23 15:06:43 -0400

    remove comments

--- components/d2l-filter-dropdown/d2l-filter-dropdown-option.js
+++ components/d2l-filter-dropdown/d2l-filter-dropdown-option.js
@@ -1,14 +1,3 @@
-/**
-`d2l-filter-dropdown-option`
-Polymer-based web component for checkbox menu items.
-
-@demo demo/checkbox-menu.html checkbox menu
-*/
-/*
-  FIXME(polymer-modulizer): the above comments were extracted
-  from HTML and may be out of place here. Review them and
-  then delete this comment!
-*/
 import '@polymer/polymer/polymer-legacy.js';
 
 import 'd2l-icons/d2l-icons.js';
@@ -21,11 +10,6 @@ const $_documentContainer = document.createElement('template');
 $_documentContainer.innerHTML = `<dom-module id="d2l-filter-dropdown-option">
 	<template strip-whitespace="">
 		<style include="d2l-menu-item-selectable-styles">
-			/*
-			 * https://github.com/Polymer/tools/issues/408
-			 * Empty style blocks break linter.
-			 */
-			:host {}
 			:host > span {
 				white-space: normal;
 			}
