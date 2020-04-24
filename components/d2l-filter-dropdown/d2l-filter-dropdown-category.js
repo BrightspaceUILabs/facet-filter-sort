1961a2bb1f18433b0d4f58e5f6ae9d788c528cfd
Dave Batiste <dave.batiste@d2l.com>
2020-04-23 23:48:52 -0400

    Update custom tab panel to Lit.

--- components/d2l-filter-dropdown/d2l-filter-dropdown-category.js
+++ components/d2l-filter-dropdown/d2l-filter-dropdown-category.js
@@ -1,164 +1,207 @@
-import { html, PolymerElement } from '@polymer/polymer/polymer-element.js';
-import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
-import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
 import '@brightspace-ui/core/components/colors/colors.js';
 import '@brightspace-ui/core/components/inputs/input-search.js';
 import '@brightspace-ui/core/components/menu/menu.js';
-import 'd2l-tabs/d2l-tab-panel-behavior.js';
-import './d2l-filter-dropdown-localize-behavior.js';
+import { css, html, LitElement } from 'lit-element/lit-element.js';
+import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { LocalizeStaticMixin } from '@brightspace-ui/core/mixins/localize-static-mixin.js';
+import { TabPanelMixin } from '@brightspace-ui/core/components/tabs/tab-panel-mixin.js';
 
-/**
- * @customElement
- * @polymer
- */
+class FilterDropdownCategory extends LocalizeStaticMixin(TabPanelMixin(LitElement)) {
 
-class D2LFilterDropdownCategory extends mixinBehaviors([D2L.PolymerBehaviors.Tabs.TabPanelBehavior, D2L.PolymerBehaviors.FilterDropdown.LocalizeBehavior], PolymerElement) {
-	static get template() {
+	static get properties() {
+		return {
+			categoryText: { type: String, attribute: 'category-text' },
+			disableSearch: { type: Boolean, attribute: 'disable-search' },
+			key: { type: String },
+			searchValue: { type: String, attribute: 'search-value' },
+			selectedOptionCount: { type: Number, attribute: 'selected-option-count' }
+		};
+	}
+
+	static get styles() {
+		return [super.styles, css`
+			:host {
+				margin: -1px -1rem 0 -1rem;
+				border-top: 1px solid var(--d2l-color-gypsum);
+				padding: 1.2rem 1rem 0 1rem;
+			}
+			.d2l-filter-dropdown-page-search {
+				margin-bottom: 0.5rem;
+			}
+			d2l-menu {
+				margin: 0 -1rem;
+				width: auto;
+			}
+		`];
+	}
+
+	static get resources() {
+		return {
+			'ar': {
+				categoryTitleMultiple: '{title} ({numSelected})',
+				searchBy: 'البحث عن {category}'
+			},
+			'de': {
+				categoryTitleMultiple: '{title} ({numSelected})',
+				searchBy: 'Nach {category} suchen'
+			},
+			'en': {
+				categoryTitleMultiple: '{title} ({numSelected})',
+				searchBy: 'Search by {category}'
+			},
+			'es': {
+				categoryTitleMultiple: '{title} ({numSelected})',
+				searchBy: 'Buscar por {category}'
+			},
+			'fi': {
+				categoryTitleMultiple: '{title} ({numSelected})',
+				searchBy: 'Hakuperuste {category}'
+			},
+			'fr': {
+				categoryTitleMultiple: '{title} ({numSelected})',
+				searchBy: 'Recherche par {category}'
+			},
+			'ja': {
+				categoryTitleMultiple: '{title} ({numSelected})',
+				searchBy: '{category} で検索'
+			},
+			'ko': {
+				categoryTitleMultiple: '{title} ({numSelected})',
+				searchBy: '{category} 필터로 검색'
+			},
+			'nb': {
+				categoryTitleMultiple: '{title} ({numSelected})',
+				searchBy: 'Søk etter {category}'
+			},
+			'nl': {
+				categoryTitleMultiple: '{title} ({numSelected})',
+				searchBy: 'Zoeken op {category}'
+			},
+			'pt': {
+				categoryTitleMultiple: '{title} ({numSelected})',
+				searchBy: 'Pesquisar por {category}'
+			},
+			'sv': {
+				categoryTitleMultiple: '{title} ({numSelected})',
+				searchBy: 'Sök per {category}'
+			},
+			'tr': {
+				categoryTitleMultiple: '{title} ({numSelected})',
+				searchBy: '{category} Kullanarak Ara'
+			},
+			'zh': {
+				categoryTitleMultiple: '{title} ({numSelected})',
+				searchBy: '按 {category} 搜索'
+			},
+			'zh-tw': {
+				categoryTitleMultiple: '{title} ({numSelected})',
+				searchBy: '依 {category} 搜尋'
+			}
+		};
+	}
+
+	constructor() {
+		super();
+		this.categoryText = '';
+		this.disableSearch = false;
+		this.key = '';
+		this.selectedOptionCount = 0;
+	}
+
+	get categoryText() {
+		return this._categoryText;
+	}
+
+	set categoryText(val) {
+		const oldVal = this._categoryText;
+		if (oldVal !== val) {
+			this._categoryText = val;
+			this.requestUpdate().then(() => {
+				this._updateTabText(this._categoryText, this.selectedOptionCount);
+			});
+		}
+	}
+
+	get selectedOptionCount() {
+		return this._selectedOptionCount;
+	}
+
+	set selectedOptionCount(val) {
+		const oldVal = this._selectedOptionCount;
+		if (oldVal !== val) {
+			this._selectedOptionCount = val;
+			this.requestUpdate().then(() => {
+				this._updateTabText(this.categoryText, this._selectedOptionCount);
+			});
+		}
+	}
+
+	firstUpdated(changedProperties) {
+		super.firstUpdated(changedProperties);
+		this.addEventListener('d2l-menu-item-select', (e) => {
+			this.selected = !this.selected;
+			this.__onSelect(e);
+		});
+	}
+
+	render() {
 		return html`
-			<style include="d2l-tab-panel-styles">
-				:host {
-					margin: -1px -1rem 0 -1rem;
-					border-top: 1px solid var(--d2l-color-gypsum);
-					padding: 1.2rem 1rem 0 1rem;
-				}
-
-				.d2l-filter-dropdown-page-search {
-					margin-bottom: 0.5rem;
-				}
-
-				d2l-menu {
-					margin: 0 -1rem;
-					width: auto;
-				}
-			</style>
-			<div class="d2l-filter-dropdown-page-search" hidden$="[[disableSearch]]">
-				<d2l-input-search placeholder="[[localize('searchBy', 'category', categoryText)]]" value="[[searchValue]]"></d2l-input-search>
+			<div class="d2l-filter-dropdown-page-search" ?hidden="${this.disableSearch}">
+				<d2l-input-search @d2l-input-search-searched="${this._handleSearchChange}"
+					placeholder="${this.localize('searchBy', 'category', this.categoryText)}"
+					value="${ifDefined(this.searchValue)}">
+				</d2l-input-search>
 			</div>
-			<d2l-menu label="[[text]]">
+			<d2l-menu @d2l-menu-item-change="${this._handleMenuItemChange}" label="${this.text}">
 				<slot></slot>
 			</d2l-menu>
 		`;
 	}
-	static get is() { return 'd2l-filter-dropdown-category'; }
-	static get properties() {
-		return {
-			key: {
-				type: String,
-				value: ''
+
+	_dispatchSelected() {
+		super._dispatchSelected();
+		this.dispatchEvent(new CustomEvent('d2l-filter-dropdown-category-selected', {
+			detail: {
+				categoryKey: this.key
 			},
-			categoryText: {
-				type: String,
-				value: ''
-			},
-			disableSearch: {
-				type: Boolean,
-				value: false
-			},
-			searchValue: {
-				type: String
-			},
-			selectedOptionCount: {
-				type: Number,
-				value: 0
-			}
-		};
-	}
-	static get observers() {
-		return [
-			'_updateTabText(categoryText, selectedOptionCount)'
-		];
-	}
-
-	ready() {
-		super.ready();
-		this._handleSearchChange = this._handleSearchChange.bind(this);
-		this._handleMenuItemChange = this._handleMenuItemChange.bind(this);
-	}
-
-	attached() {
-		afterNextRender(this, function() {
-			var menu = this._getMenu();
-			var search = this._getSearchInput();
-
-			search.addEventListener('d2l-input-search-searched', this._handleSearchChange);
-			menu.addEventListener('d2l-menu-item-change', this._handleMenuItemChange);
-		}.bind(this));
-	}
-
-	detached() {
-		var menu = this._getMenu();
-		var search = this._getSearchInput();
-
-		search.removeEventListener('d2l-input-search-searched', this._handleSearchChange);
-		menu.removeEventListener('d2l-menu-item-change', this._handleMenuItemChange);
-	}
-
-	_getSearchInput() {
-		return this.shadowRoot.querySelector('d2l-input-search');
-	}
-
-	_getMenu() {
-		return this.shadowRoot.querySelector('d2l-menu');
-	}
-
-	_handleSearchChange(e) {
-		e.stopPropagation();
-		this.dispatchEvent(
-			new CustomEvent(
-				'd2l-filter-dropdown-category-searched',
-				{
-					detail: {
-						categoryKey: this.key,
-						value: e.detail.value
-					},
-					composed: true,
-					bubbles: true
-				}
-			)
-		);
+			bubbles: true,
+			composed: true
+		}));
 	}
 
 	_handleMenuItemChange(e) {
 		e.stopPropagation();
-		this.dispatchEvent(
-			new CustomEvent(
-				'd2l-filter-dropdown-option-change',
-				{
-					detail: {
-						categoryKey: this.key,
-						menuItemKey: e.detail.value,
-						selected: e.detail.selected
-					},
-					composed: true,
-					bubbles: true
-				}
-			)
-		);
+		this.dispatchEvent(new CustomEvent('d2l-filter-dropdown-option-change', {
+			detail: {
+				categoryKey: this.key,
+				menuItemKey: e.detail.value,
+				selected: e.detail.selected
+			},
+			composed: true,
+			bubbles: true
+		}));
 	}
 
-	_dispatchSelected() {
-		super._dispatchSelected();
-		this.dispatchEvent(
-			new CustomEvent(
-				'd2l-filter-dropdown-category-selected',
-				{
-					detail: {
-						categoryKey: this.key
-					},
-					bubbles: true,
-					composed: true
-				}
-			)
-		);
+	_handleSearchChange(e) {
+		e.stopPropagation();
+		this.dispatchEvent(new CustomEvent('d2l-filter-dropdown-category-searched', {
+			detail: {
+				categoryKey: this.key,
+				value: e.detail.value
+			},
+			composed: true,
+			bubbles: true
+		}));
 	}
 
 	_updateTabText(categoryText, selectedOptionCount) {
 		if (selectedOptionCount === 0) {
-			this.text = categoryText;
+			this.setAttribute('text', categoryText);
 		} else {
-			this.text = this.localize('categoryTitleMultiple', 'title', categoryText, 'numSelected', selectedOptionCount);
+			this.setAttribute('text', this.localize('categoryTitleMultiple', 'title', categoryText, 'numSelected', selectedOptionCount));
 		}
 	}
+
 }
 
-window.customElements.define(D2LFilterDropdownCategory.is, D2LFilterDropdownCategory);
+customElements.define('d2l-filter-dropdown-category', FilterDropdownCategory);
--- components/d2l-filter-dropdown/d2l-filter-dropdown-localize-behavior.js
+++ components/d2l-filter-dropdown/d2l-filter-dropdown-localize-behavior.js
@@ -18,139 +18,109 @@ D2L.PolymerBehaviors.FilterDropdown.LocalizeBehaviorImpl = {
 			value: function() {
 				return {
 					'ar': {
-						categoryTitleMultiple: '{title} ({numSelected})',
 						clear: 'مسح',
 						filter: 'التصفية',
 						filterBy: 'تصفية بحسب',
 						filterMultiple: 'Filter: {numOptions} Filters',
-						filterSingle: 'التصفية: عامل تصفية واحد',
-						searchBy: 'البحث عن {category}'
+						filterSingle: 'التصفية: عامل تصفية واحد'
 					},
 					'de': {
-						categoryTitleMultiple: '{title} ({numSelected})',
 						clear: 'Löschen',
 						filter: 'Filter',
 						filterBy: 'Filtern nach',
 						filterMultiple: 'Filter: {numOptions} Filter',
-						filterSingle: 'Filter: 1 Filter',
-						searchBy: 'Nach {category} suchen'
+						filterSingle: 'Filter: 1 Filter'
 					},
 					'en': {
-						categoryTitleMultiple: '{title} ({numSelected})',
 						clear: 'Clear',
 						filter: 'Filter',
 						filterBy: 'Filter By',
 						filterMultiple: 'Filter: {numOptions} Filters',
-						filterSingle: 'Filter: 1 Filter',
-						searchBy: 'Search by {category}'
+						filterSingle: 'Filter: 1 Filter'
 					},
 					'es': {
-						categoryTitleMultiple: '{title} ({numSelected})',
 						clear: 'Borrar',
 						filter: 'Filtrar',
 						filterBy: 'Filtrar por',
 						filterMultiple: 'Filtro: {numOptions} filtros',
-						filterSingle: 'Filtro: 1 filtro',
-						searchBy: 'Buscar por {category}'
+						filterSingle: 'Filtro: 1 filtro'
 					},
 					'fi': {
-						categoryTitleMultiple: '{title} ({numSelected})',
 						clear: 'Tyhjennä',
 						filter: 'Suodatin',
 						filterBy: 'Suodatusperuste',
 						filterMultiple: 'Suodatus: {numOptions} suodatinta',
-						filterSingle: 'Suodatus: 1 suodatin',
-						searchBy: 'Hakuperuste {category}'
+						filterSingle: 'Suodatus: 1 suodatin'
 					},
 					'fr': {
-						categoryTitleMultiple: '{title} ({numSelected})',
 						clear: 'Effacer',
 						filter: 'Filtrer',
 						filterBy: 'Filtrer par',
 						filterMultiple: 'Filtre : {numOptions} filtres',
-						filterSingle: 'Filtre : 1 filtre',
-						searchBy: 'Recherche par {category}'
+						filterSingle: 'Filtre : 1 filtre'
 					},
 					'ja': {
-						categoryTitleMultiple: '{title} ({numSelected})',
 						clear: 'クリア',
 						filter: 'フィルタ',
 						filterBy: 'フィルタの条件',
 						filterMultiple: 'フィルタ数: {numOptions}',
-						filterSingle: 'フィルタ数: 1',
-						searchBy: '{category} で検索'
+						filterSingle: 'フィルタ数: 1'
 					},
 					'ko': {
-						categoryTitleMultiple: '{title} ({numSelected})',
 						clear: '지우기',
 						filter: '필터',
 						filterBy: '필터링 기준',
 						filterMultiple: '필터: {numOptions}개 필터',
-						filterSingle: '필터: 1개 필터',
-						searchBy: '{category} 필터로 검색'
+						filterSingle: '필터: 1개 필터'
 					},
 					'nb': {
-						categoryTitleMultiple: '{title} ({numSelected})',
 						clear: 'Fjern',
 						filter: 'Filter',
 						filterBy: 'Filtrer etter',
 						filterSingle: 'Filter: 1 filter',
-						filterMultiple: 'Filter: {numOptions} filtre',
-						searchBy: 'Søk etter {category}'
+						filterMultiple: 'Filter: {numOptions} filtre'
 					},
 					'nl': {
-						categoryTitleMultiple: '{title} ({numSelected})',
 						clear: 'Wissen',
 						filter: 'Filteren',
 						filterBy: 'Filteren op',
 						filterMultiple: 'Filteren: {numOptions} filters',
-						filterSingle: 'Filteren: 1 filter',
-						searchBy: 'Zoeken op {category}'
+						filterSingle: 'Filteren: 1 filter'
 					},
 					'pt': {
-						categoryTitleMultiple: '{title} ({numSelected})',
 						clear: 'Apagar',
 						filter: 'Filtro',
 						filterBy: 'Filtrar por',
 						filterMultiple: 'Filtrar: {numOptions} filtros',
-						filterSingle: 'Filtrar: 1 Filtro',
-						searchBy: 'Pesquisar por {category}'
+						filterSingle: 'Filtrar: 1 Filtro'
 					},
 					'sv': {
-						categoryTitleMultiple: '{title} ({numSelected})',
 						clear: 'Rensa',
 						filter: 'Filter',
 						filterBy: 'Filtrera efter',
 						filterMultiple: 'Filter: {numOptions} filter',
-						filterSingle: 'Filter: 1 filter',
-						searchBy: 'Sök per {category}'
+						filterSingle: 'Filter: 1 filter'
 					},
 					'tr': {
-						categoryTitleMultiple: '{title} ({numSelected})',
 						clear: 'Temizle',
 						filter: 'Filtrele',
 						filterBy: 'Filtreleme Ölçütü',
 						filterMultiple: 'Filtre: {numOptions} Filtre',
-						filterSingle: 'Filtre: 1 Filtre',
-						searchBy: '{category} Kullanarak Ara'
+						filterSingle: 'Filtre: 1 Filtre'
 					},
 					'zh': {
-						categoryTitleMultiple: '{title} ({numSelected})',
 						clear: '清除',
 						filter: '筛选',
 						filterBy: '筛选条件',
 						filterMultiple: '筛选：{numOptions} 个筛选条件',
-						filterSingle: '筛选：1 个筛选条件',
-						searchBy: '按 {category} 搜索'
+						filterSingle: '筛选：1 个筛选条件'
 					},
 					'zh-tw': {
-						categoryTitleMultiple: '{title} ({numSelected})',
 						clear: '清除',
 						filter: '篩選',
 						filterBy: '篩選依據',
 						filterMultiple: '篩選器：{numOptions} 個篩選器',
-						filterSingle: '篩選器：1 個篩選器',
-						searchBy: '依 {category} 搜尋'
+						filterSingle: '篩選器：1 個篩選器'
 					}
 				};
 			}
