dc4be495c6aa20a8ff16848dc1d007f8e377afc5
JenkinsReader <resupport@desire2learn.com>
2022-04-29 06:22:57 -0400

    Translation Updates

---
+++ lang/fr-on.js
@@ -0,0 +1,23 @@
+/* eslint quotes: 0 */
+
+export default {
+	"appliedFilters": "Filtres appliqués :",
+	"noActiveFilters": "Aucun filtre actif",
+	"filterRemoved": "Filtre {filterText} retiré",
+	"clearFilters": "Effacer les filtres",
+	"allFiltersRemoved": "Tous les filtres sont retirés",
+	"categoryTitleMultiple": "{title} ({numSelected})",
+	"searchBy": "Rechercher par {category}",
+	"clear": "Effacer",
+	"filter": "Filtrer",
+	"filterBy": "Filtrer à l’aide de",
+	"filterMultiple": "Filtre : Filtres {numOptions}",
+	"filterSingle": "Filtre : 1 filtre",
+	"more": "Plus",
+	"searchPagedResults": "{rangeStart} – {rangeEnd} de {totalCount} résultats",
+	"searchPagedResultsForQuery": "{rangeStart} – {rangeEnd} de {totalCount} résultats pour « {query} »",
+	"searchResults": "{totalCount} résultats",
+	"searchResultsForQuery": "{totalCount} résultats pour « {query} »",
+	"sort": "Trier",
+	"sortWithOption": "Trier : {option}"
+};
