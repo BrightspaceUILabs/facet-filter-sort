c3958300a5c28c092d37ae3411c4a48023e50c4d
tie.d2l.dev <TieUser@d2l.com>
2024-01-22 16:10:31 +0000

    Translation Updates

---
+++ lang/en-gb.js
@@ -0,0 +1,18 @@
+/* eslint quotes: 0 */
+
+export default {
+	"appliedFilters": "Applied Filters:",
+	"noActiveFilters": "No active filters",
+	"filterRemoved": "Filter {filterText} removed",
+	"clearFilters": "Clear filters",
+	"allFiltersRemoved": "All filters removed",
+	"categoryTitleMultiple": "{title} ({numSelected})",
+	"searchBy": "Search by {category}",
+	"clear": "Clear",
+	"filter": "Filter",
+	"filterBy": "Filter By",
+	"filterMultiple": "Filter: {numOptions} Filters",
+	"filterSingle": "Filter: 1 Filter",
+	"sort": "Sort",
+	"sortWithOption": "Sort: {option}"
+};
