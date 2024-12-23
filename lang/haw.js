606cbf088497e3359dd013b646aa83c7c6100e3a
tie.d2l.dev <TieUser@d2l.com>
2024-12-23 18:09:51 +0000

    Translation Updates

---
+++ lang/haw.js
@@ -0,0 +1,16 @@
+/* eslint quotes: 0 */
+
+export default {
+	"appliedFilters": "Nā kānana i hoʻohana ʻia:",
+	"noActiveFilters": "ʻAʻohe kānana hana",
+	"clearFilters": "Holoi i nā kānana",
+	"categoryTitleMultiple": "{title} ({numSelected})",
+	"searchBy": "Huli ma {category}",
+	"clear": "Akaka",
+	"filter": "Kānana",
+	"filterBy": "kānana e",
+	"filterMultiple": "Kānana: {numOptions} kānana",
+	"filterSingle": "Kānana: 1 kānana",
+	"sort": "ʻano ʻano",
+	"sortWithOption": "Hoʻokaʻawale: {koho}"
+};
