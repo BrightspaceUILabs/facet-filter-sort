edf0ced0e6f84c06bd63d758e978a627dab4bded
tie.d2l.dev <TieUser@d2l.com>
2025-04-08 17:42:57 +0000

    Translation Updates

---
+++ lang/mi.js
@@ -0,0 +1,16 @@
+/* eslint quotes: 0 */
+
+export default {
+	"appliedFilters": "Ngā Tātari Hoatu:",
+	"noActiveFilters": "Kāore he tātari hohe",
+	"clearFilters": "Whakawātea ngā tātari",
+	"categoryTitleMultiple": "{title} ({numSelected})",
+	"searchBy": "Rapu mā {category}",
+	"clear": "Mahea",
+	"filter": "Tātari",
+	"filterBy": "Tātari mā:",
+	"filterMultiple": "Filter: {numOptions} Filters",
+	"filterSingle": "Tātari: 1 Tātari",
+	"sort": "Kōmaka",
+	"sortWithOption": "Kōmaka: {option}"
+};
