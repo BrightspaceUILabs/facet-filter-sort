e3f7d93739a51ae9c1c0eb72aa3bd70ac1983275
tie.d2l.dev <TieUser@d2l.com>
2025-09-03 15:19:00 +0000

    Translation Updates

---
+++ lang/th.js
@@ -0,0 +1,16 @@
+/* eslint quotes: 0 */
+
+export default {
+	"appliedFilters": "ตัวกรองที่ใช้:",
+	"noActiveFilters": "ไม่มีตัวกรองที่ใช้อยู่",
+	"clearFilters": "ล้างตัวกรอง",
+	"categoryTitleMultiple": "{title} ({numSelected})",
+	"searchBy": "ค้นหาตาม {category}",
+	"clear": "ล้าง",
+	"filter": "ตัวกรอง",
+	"filterBy": "กรองตาม",
+	"filterMultiple": "ตัวกรอง: {numOptions} ตัวกรอง",
+	"filterSingle": "ตัวกรอง: 1 ตัวกรอง",
+	"sort": "เรียงลำดับ",
+	"sortWithOption": "เรียงลำดับ: {option}"
+};
